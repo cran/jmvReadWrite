c3118c11c74c87838bd1fe8aeb5ce9a4c2430664
Sebastian Jentschke <sebastian.jentschke@uib.no>
2022-02-13 02:10:02 +0000

    version 0.3.0

--- inst/doc/jmvReadWrite.R
+++ inst/doc/jmvReadWrite.R
@@ -21,31 +21,38 @@ library(jmvReadWrite);
 library(jmv);
 
 fleOMV = system.file("extdata", "ToothGrowth.omv", package = "jmvReadWrite");
-data = read_omv(fleNme = fleOMV);
-jmv::ANOVA(
-    formula = len ~ supp + dose + supp:dose,
-    data = data,
-    effectSize = c("omega"),
-    modelTest = TRUE,
-    homo = TRUE,
-    norm = TRUE);
+data = read_omv(fleOMV);
+# if the "jmv"-package is installed, we can run a test analysis with the data
+if ("jmv" %in% rownames(installed.packages())) {
+    jmv::ANOVA(
+        formula = len ~ supp + dose + supp:dose,
+        data = data,
+        effectSize = c("omega"),
+        modelTest = TRUE,
+        homo = TRUE,
+        norm = TRUE);
+    }
 
 ## ---- echo=TRUE---------------------------------------------------------------
 library(jmvReadWrite);
 fleOMV = system.file("extdata", "ToothGrowth.omv", package = "jmvReadWrite");
-data = read_omv(fleNme = fleOMV, getSyn = TRUE);
+data = read_omv(fleOMV, getSyn = TRUE);
+# shows the syntax of the analyses from the .omv-file
 # please note that syntax extraction may not work on all systems
 # if the syntax couldn't be extracted, an empty list (length = 0) is returned,
 # otherwise, the syntax of the analyses from the .omv-file is shown and  
 # the commands of the first and the second analysis are run, with the
 # output of the second analysis assigned to the variable result2
-if (length(attr(data, 'syntax')) >= 2) {
-    attr(data, 'syntax')
-    eval(parse(text=attr(data, 'syntax')[[1]]))
-    eval(parse(text=paste0('result2 = ', attr(data, 'syntax')[[2]])))
-    names(result2)
-    # -> "main"      "assump"    "contrasts" "postHoc"   "emm"
-    # (the names of the five output tables)
+if (length(attr(data, "syntax")) >= 2) {
+    attr(data, "syntax")
+    # if the "jmv"-package is installed, we can run the analyses in "syntax"     
+    if ("jmv" %in% rownames(installed.packages())) {
+        eval(parse(text=attr(data, "syntax")[[1]]))
+        eval(parse(text=paste0("result2 = ", attr(data, "syntax")[[2]])))
+        names(result2)
+        # -> "main"      "assump"    "contrasts" "postHoc"   "emm"
+        # (the names of the five output tables)
+    }
 }
 
 ## ---- echo=TRUE---------------------------------------------------------------
@@ -74,7 +81,7 @@ unlink("Trial.omv");
 # essential meta-data to ensure that the written file looks and works like the
 # original file (plus you modifications)
 fleOMV = system.file("extdata", "ToothGrowth.omv", package = "jmvReadWrite");
-data = read_omv(fleNme = fleOMV, sveAtt = TRUE);
+data = read_omv(fleOMV, sveAtt = TRUE);
 # shows the names of the attributes for the whole data set (e.g., number of
 # rows and columns) and the names of the attributes of the first column
 names(attributes(data));
